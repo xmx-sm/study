927356cf99b00505e9169392d77f6c2630b42d63
xmx-sm <15033381268@139.com>
2025-01-05 00:06:09 +0800

    文件上传

--- fastapi/fasta/main.py
+++ fastapi/fasta/main.py
@@ -3,8 +3,8 @@ from fastapi.templating import Jinja2Templates
 from shou.shou_app import app as shou
 from fu.fu_app import app as fu
 import uvicorn
-from Sqllog import TORTOISE_ORM
-from tortoise.contrib.fastapi import register_tortoise
+# from Sqllog import TORTOISE_ORM
+# from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -25,12 +25,12 @@ def add_file(request: Request):
 
 
 # 假设 app 是一个 FastAPI 应用实例
-register_tortoise(
-    app=app,  # 将 Tortoise ORM 注册到 FastAPI 应用实例上
-    config=TORTOISE_ORM,  # 使用 TORTOISE_ORM 字典作为配置项来初始化 Tortoise ORM
-    # generate_schemas=True,  # 可选：如果需要在应用启动时自动生成数据库模式，请取消此行注释
-    # add_exception_handlers=True,  # 可选：添加异常处理器以处理 Tortoise ORM 引发的异常
-)
+# register_tortoise(
+#     app=app,  # 将 Tortoise ORM 注册到 FastAPI 应用实例上
+#     config=TORTOISE_ORM,  # 使用 TORTOISE_ORM 字典作为配置项来初始化 Tortoise ORM
+#     # generate_schemas=True,  # 可选：如果需要在应用启动时自动生成数据库模式，请取消此行注释
+#     # add_exception_handlers=True,  # 可选：添加异常处理器以处理 Tortoise ORM 引发的异常
+# )
 
 if __name__ == "__main__":
     uvicorn.run(app,host="127.0.0.1",port=8000)
